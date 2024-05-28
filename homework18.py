095ae8209273f66dcb7409c1ea275ead52591dca
al3xx4nax <121611925+al3xx4nax@users.noreply.github.com>
2024-05-28 18:55:25 +0300

    Update homework18.py
    new changes

--- homework18.py
+++ homework18.py
@@ -1,18 +1,19 @@
 class House:
     def __init__(self, name, number_of_floors):
-            self.name = name
-            self.number_of_floors = number_of_floors
+        self.new_floor = None
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        self.new_floor = new_floor
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print("Такого этажа не существует!")
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
 
 
-def go_to():
-    new_floor = int(input())
-    if new_floor < 1 or new_floor > house.number_of_floors:
-        print("Такого этажа не существует!")
-    else:
-        for i in range(1, new_floor + 1):
-            print(i)
-
-
-house = House("ЖК Румянцево-парк", 21)
-
-go_to()
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
